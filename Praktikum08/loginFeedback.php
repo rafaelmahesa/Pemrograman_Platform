7fde766d886618a9f792cad7b11b1a11f193c531
Rafael Mahesa <mahesaputra740@gmail.com>
2022-11-09 17:34:10 +0700

    loginfeedback.php hapus if isset username

--- Praktikum08/loginFeedback.php
+++ Praktikum08/loginFeedback.php
@@ -1,9 +1,4 @@
-<?php
-    session_start();
-    if(! isset($_SESSION['username'])){
-        header("Location: login.php");
-    }
-?>
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
